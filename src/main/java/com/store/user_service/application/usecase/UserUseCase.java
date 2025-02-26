c396ebc6bf5e1c56796d7b80260efcd65f18f1fa
htreb <htrebor20@mail.com>
2025-02-26 13:17:20 -0500

    add age validation

--- src/main/java/com/store/user_service/application/usecase/UserUseCase.java
+++ src/main/java/com/store/user_service/application/usecase/UserUseCase.java
@@ -1,9 +1,14 @@
 package com.store.user_service.application.usecase;
 
+import com.store.user_service.domain.constants.Constants;
+import com.store.user_service.domain.exception.BadRequestValidationException;
 import com.store.user_service.domain.model.User;
 import com.store.user_service.domain.ports.in.IUserServicePort;
 import com.store.user_service.domain.ports.out.IUserPersistencePort;
 import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.Period;
 
 @RequiredArgsConstructor
 public class UserUseCase implements IUserServicePort {
@@ -11,6 +16,21 @@ public class UserUseCase implements IUserServicePort {
 
     @Override
     public User create(User user) {
+        validateAge(user);
+
         return userPersistencePort.saveUser(user);
     }
+
+    private void validateAge(User user) {
+        LocalDate currentDate = LocalDate.now();
+        if (user.getBirthdate() == null) {
+            throw new BadRequestValidationException(Constants.BIRTHDATE_MANDATORY_EXCEPTION_MESSAGE);
+        }
+        int age = Period.between(user.getBirthdate(), currentDate).getYears();
+        if (age < 18) {
+            throw new BadRequestValidationException(Constants.AGE_VALIDATIONS_EXCEPTION_MESSAGE);
+        }
+    }
+
 }
+
---
+++ src/main/java/com/store/user_service/domain/constants/Constants.java
@@ -0,0 +1,12 @@
+package com.store.user_service.domain.constants;
+
+public final class Constants {
+    private Constants() {
+
+    }
+
+    public static final String BIRTHDATE_MANDATORY_EXCEPTION_MESSAGE = "The user birthdate is mandatory.";
+    public static final String AGE_VALIDATIONS_EXCEPTION_MESSAGE = "The user must be an adult.";
+
+
+}
