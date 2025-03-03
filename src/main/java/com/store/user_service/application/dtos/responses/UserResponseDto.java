01853cb9a11edc19ee892ab5c2618499e2a4e4c0
htreb <htrebor20@mail.com>
2025-03-02 22:06:17 -0500

    Implemented Swagger UI with Springdoc OpenAPI to document the API endpoints.

--- src/main/java/com/store/user_service/application/dtos/responses/UserResponseDto.java
+++ src/main/java/com/store/user_service/application/dtos/responses/UserResponseDto.java
@@ -2,9 +2,11 @@ package com.store.user_service.application.dtos.responses;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 
 public class UserResponseDto {
     private String name;
--- src/main/java/com/store/user_service/infrastructure/controllers/UserController.java
+++ src/main/java/com/store/user_service/infrastructure/controllers/UserController.java
@@ -5,23 +5,49 @@ import com.store.user_service.application.dtos.responses.UserResponseDto;
 import com.store.user_service.application.mappers.IUserRequestMapper;
 import com.store.user_service.domain.model.User;
 import com.store.user_service.domain.ports.in.IUserServicePort;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
+@Tag(name = "Users", description = "Operations related to user management")
+
 public class UserController {
     private final IUserRequestMapper userRequestMapper;
     private final IUserServicePort userServicePort;
 
     @PostMapping("/")
+    @Operation(
+            summary = "Create a new user",
+            description = "This endpoint allows you to create a user in the system."
+    )
+    @RequestBody(
+            required = true,
+            content = @Content(
+                    schema = @Schema(
+                            implementation = UserRequestDto.class,
+                            description = "User data required for registration"
+                    )
+            )
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "User successfully created",
+                    content = @Content(schema = @Schema(implementation = UserResponseDto.class))),
+            @ApiResponse(responseCode = "400", description = "Bad request (invalid data)")
+    })
     public ResponseEntity<UserResponseDto> createUser(@RequestBody @Valid UserRequestDto request) {
         User user = userRequestMapper.toModel(request);
         User user1 = userServicePort.create(user);
