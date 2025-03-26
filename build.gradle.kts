b3703376832b5c46b34c8b3a7fdbf1f001010ebe
htreb <htrebor20@mail.com>
2025-03-26 16:41:16 -0500

    Add feing client

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ version = "0.0.1-SNAPSHOT"
 
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
+        languageVersion.set(JavaLanguageVersion.of(17))
     }
 }
 
@@ -23,26 +23,45 @@ repositories {
     mavenCentral()
 }
 
+ext {
+    set("springCloudVersion", "2023.0.2")
+}
+
 dependencies {
+    // Spring Boot dependencies
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.mapstruct:mapstruct:1.5.5.Final")
     implementation("org.springframework.boot:spring-boot-starter-mail:3.4.3")
+
+    // Spring Cloud dependencies
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign:4.2.1")
+
+    // MapStruct for object mapping
+    implementation("org.mapstruct:mapstruct:1.5.5.Final")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+    annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
+
+    // Lombok (for reducing boilerplate code)
     compileOnly("org.projectlombok:lombok")
-    runtimeOnly("com.mysql:mysql-connector-j")
     annotationProcessor("org.projectlombok:lombok")
+
+    // Database driver
+    runtimeOnly("com.mysql:mysql-connector-j")
+
+    // Testing dependencies
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    dependencies {
-        annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
+}
+
+dependencyManagement {
+    val springCloudVersion = "2023.0.2" // Definir variable correctamente
+
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion")
     }
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
 }
-
-
-
