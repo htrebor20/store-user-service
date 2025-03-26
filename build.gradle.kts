964bd0a905cfdd9c2adee887a3cd9dbd8c66e011
htreb <htrebor20@mail.com>
2025-03-26 16:43:26 -0500

    Add openfeign

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,34 +28,24 @@ ext {
 }
 
 dependencies {
-    // Spring Boot dependencies
+
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-mail:3.4.3")
-
-    // Spring Cloud dependencies
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign:4.2.1")
-
-    // MapStruct for object mapping
     implementation("org.mapstruct:mapstruct:1.5.5.Final")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
     annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
-
-    // Lombok (for reducing boilerplate code)
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
-
-    // Database driver
     runtimeOnly("com.mysql:mysql-connector-j")
-
-    // Testing dependencies
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 dependencyManagement {
-    val springCloudVersion = "2023.0.2" // Definir variable correctamente
+    val springCloudVersion = "2023.0.2"
 
     imports {
         mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion")
